d1776fbcd2be2b2afc6685cdae60944ff9d1435b
sOliveto93 <s.oliveto93@gmail.com>
2026-08-24 20:33:54 -0300

    feat: integracion rama tute

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import datos.Festival;
@@ -10,9 +11,11 @@ public class Main {
     public static void main(String[] args) {
 
         FestivalABM festivalABM = new FestivalABM();
-
+        Festival festival1=new Festival("la fiesta del dulce de leche", "primavera", LocalDate.of(2026,9,21), LocalDate.of(2026, 9, 23), 650, 300 ,50, 100);
+        festivalABM.agregarFestival(festival1);
         List<Festival> festivales = festivalABM.traerTodos();
-
+        
+        System.out.println("Festivales registrados: "+festivales.size());
         for (Festival festival : festivales) {
             System.out.println(festival);
         }
